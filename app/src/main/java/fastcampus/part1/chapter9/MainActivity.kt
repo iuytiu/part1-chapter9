c5b55ae76d6b70d4368c3cf30cc8851b13cc7ac5
iuyt <itsyksj@gmail.com>
2024-06-24 23:46:32 +0900

    Feat:  Implement screen connection from Bottom Navigation
    - Click the Bottom Navigation icon to connect to each Fragment.
    - Set up app start screen(default screen).

--- app/src/main/java/fastcampus/part1/chapter9/MainActivity.kt
+++ app/src/main/java/fastcampus/part1/chapter9/MainActivity.kt
@@ -3,18 +3,44 @@ package fastcampus.part1.chapter9
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import fastcampus.part1.chapter9.databinding.ActivityMainBinding
+import fastcampus.part1.chapter9.navigation.FeedFragment
+import fastcampus.part1.chapter9.navigation.HomeFragment
+import fastcampus.part1.chapter9.navigation.LibraryFragment
+import fastcampus.part1.chapter9.navigation.ProfileFragment
+import fastcampus.part1.chapter9.navigation.SearchFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //앱 상단 상태바(status bar) 제거
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        //시작화면 지정
+        replaceFragment(HomeFragment())
+
+        //아이콘을 통해 각 Fragment로 화면이동
+        binding.bottomNavigation.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.menu_home -> replaceFragment(HomeFragment())
+                R.id.menu_feed -> replaceFragment(FeedFragment())
+                R.id.menu_library -> replaceFragment(LibraryFragment())
+                R.id.menu_search -> replaceFragment(SearchFragment())
+                R.id.menu_profile -> replaceFragment(ProfileFragment())
+                else -> throw IllegalArgumentException("not found itemId")
+            }
+            true
         }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().replace(R.id.fragment, fragment).commit()
     }
 }
